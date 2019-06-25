e011fb184d49d42e657b750d127d8a8ca0f2636b
Landry Irakoze <irakozel@gmail.com>
2019-06-25 17:46:30 -0400

    added carousel

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
-class Carousel {
-    constructor(element) {
-        this.element = element;
+// class MyCarousel {
+//     constructor(element) {
+//         this.element = element;
         
-        this.images = Array.from(this.element.querySelectorAll('img'));
+//         this.images = Array.from(this.element.querySelectorAll('img'));
 
-        this.leftButton = this.element.querySelector('.left-button');
-        this.rightButton = this.element.querySelector('.right-button');
+//         this.leftButton = this.element.querySelector('.left-button');
+//         this.rightButton = this.element.querySelector('.right-button');
 
-        this.leftButton.addEventListener('click', () => this.slideLeft());
-        this.rightButton.addEventListener('click', () => this.slideRight());
+//         this.leftButton.addEventListener('click', () => this.slideLeft());
+//         this.rightButton.addEventListener('click', () => this.slideRight());
 
-        // this.test = [1, 2, 3, 4]
-    }
+//         // this.test = [1, 2, 3, 4]
+//     }
     
-    slideRight() {
-        // let images = Array.from(this.element.querySelectorAll('img'));
-        // console.log(images);
-        // console.log(this.images);
+//     slideRight() {
+//         // let images = Array.from(this.element.querySelectorAll('img'));
+//         // console.log(images);
+//         // console.log(this.images);
 
-        this.images.forEach(item => item.style.display = 'none');
+//         this.images.forEach(item => item.style.display = 'none');
 
-        this.images.unshift(this.images[this.images.length -1]);
-        this.images.pop();
+//         this.images.unshift(this.images[this.images.length -1]);
+//         this.images.pop();
 
-        this.images[0].style.display = 'block';
+//         this.images[0].style.display = 'block';
 
-        // console.log(this.test);
-        // this.test.unshift(this.test[this.test.length -1]);
-        // this.test.pop();
-        // console.log(this.test);
-    }
+//         // console.log(this.test);
+//         // this.test.unshift(this.test[this.test.length -1]);
+//         // this.test.pop();
+//         // console.log(this.test);
+//     }
 
-    slideLeft() {
+//     slideLeft() {
+//         console.log('click');
+//         this.images.forEach(item => item.style.display = 'none');
 
-        this.images.forEach(item => item.style.display = 'none');
+//         this.images.push(this.images[0]);
+//         this.images.shift();
 
-        this.images.push(this.images[0]);
-        this.images.shift();
+//         this.images[0].style.display = 'block';
 
-        this.images[0].style.display = 'block';
+//         // console.log(this.test);
+//         // this.test.push(this.test[0]);
+//         // this.test.shift();
+//         // console.log(this.test);
+//     }
+// }
 
-        // console.log(this.test);
-        // this.test.push(this.test[0]);
-        // this.test.shift();
-        // console.log(this.test);
-    }
-}
+// let carousell = document.querySelector('.mycarousel');
+// let myCarousel = new MyCarousel(carousell);
 
-let carousel = document.querySelector('.carousel');
-let myCarousel = new Carousel(carousel);
+// console.log(carousel);
+
