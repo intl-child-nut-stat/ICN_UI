0d49f03003e6044237e38f826189268d2e0edd5b
Landry Irakoze <irakozel@gmail.com>
2019-06-25 18:56:40 -0400

    tested carousel

--- index.js
+++ index.js
@@ -52,3 +52,55 @@
 
 // console.log(carousel);
 
+class Carousel {
+    constructor(carousel) {
+        this.carousel = carousel;
+        console.log(this.carousel);
+        this.images = this.carousel.querySelectorAll('img');
+        this.index = 0;
+        this.current = new CarouselImage(this.images);
+        this.leftButton = this.carousel.querySelector('.left-button');
+        this.rightButton = this.carousel.querySelector('.right-button');
+        this.leftButton.addEventListener('click', (e) => this.displayImage(e));
+        this.rightButton.addEventListener('click', (e) => this.displayImage(e));
+    }
+    displayImage(e) {
+        if (e.target.className == 'right-button') {
+            if (this.index < this.images.length - 1) {
+                this.index += 1;
+            }
+            else {
+                this.index = 0;
+ 
+            }
+        }
+        if (e.target.className == 'left-button') {
+            if (this.index > 0) {
+                this.index -= 1;
+            }
+            else {
+                this.index = this.images.length - 1;
+            }
+        }
+        this.current.displayNewImage(this.index);
+ 
+ 
+    }
+ }
+ 
+ class CarouselImage {
+    constructor(images) {
+        this.images = images;
+        this.images[0].style.display = "block";
+ 
+ 
+    }
+    displayNewImage(index) {
+        Array.from(this.images).forEach(image => image.style.display = "none");
+        this.images[index].style.display = "block";
+    }
+ }
+ 
+ let carousel = document.querySelector('.carousel');
+ let carouselObject = new Carousel(carousel);
+ 
