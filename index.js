3d4c7a4c9125def8318bf18669528946a50a4903
Landry Irakoze <irakozel@gmail.com>
2019-06-25 20:09:39 -0400

    added nikki pic

--- index.js
+++ index.js
@@ -1,106 +1,29 @@
-// class MyCarousel {
-//     constructor(element) {
-//         this.element = element;
-        
-//         this.images = Array.from(this.element.querySelectorAll('img'));
+var slideIndex = 1;
+showSlides(slideIndex);
 
-//         this.leftButton = this.element.querySelector('.left-button');
-//         this.rightButton = this.element.querySelector('.right-button');
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-//         this.leftButton.addEventListener('click', () => this.slideLeft());
-//         this.rightButton.addEventListener('click', () => this.slideRight());
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-//         // this.test = [1, 2, 3, 4]
-//     }
-    
-//     slideRight() {
-//         // let images = Array.from(this.element.querySelectorAll('img'));
-//         // console.log(images);
-//         // console.log(this.images);
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
 
-//         this.images.forEach(item => item.style.display = 'none');
-
-//         this.images.unshift(this.images[this.images.length -1]);
-//         this.images.pop();
-
-//         this.images[0].style.display = 'block';
-
-//         // console.log(this.test);
-//         // this.test.unshift(this.test[this.test.length -1]);
-//         // this.test.pop();
-//         // console.log(this.test);
-//     }
-
-//     slideLeft() {
-//         console.log('click');
-//         this.images.forEach(item => item.style.display = 'none');
-
-//         this.images.push(this.images[0]);
-//         this.images.shift();
-
-//         this.images[0].style.display = 'block';
-
-//         // console.log(this.test);
-//         // this.test.push(this.test[0]);
-//         // this.test.shift();
-//         // console.log(this.test);
-//     }
-// }
-
-// let carousell = document.querySelector('.mycarousel');
-// let myCarousel = new MyCarousel(carousell);
-
-// console.log(carousel);
-
-class Carousel {
-    constructor(carousel) {
-        this.carousel = carousel;
-        console.log(this.carousel);
-        this.images = this.carousel.querySelectorAll('img');
-        this.index = 0;
-        this.current = new CarouselImage(this.images);
-        this.leftButton = this.carousel.querySelector('.left-button');
-        this.rightButton = this.carousel.querySelector('.right-button');
-        this.leftButton.addEventListener('click', (e) => this.displayImage(e));
-        this.rightButton.addEventListener('click', (e) => this.displayImage(e));
-    }
-    displayImage(e) {
-        if (e.target.className == 'right-button') {
-            if (this.index < this.images.length - 1) {
-                this.index += 1;
-            }
-            else {
-                this.index = 0;
- 
-            }
-        }
-        if (e.target.className == 'left-button') {
-            if (this.index > 0) {
-                this.index -= 1;
-            }
-            else {
-                this.index = this.images.length - 1;
-            }
-        }
-        this.current.displayNewImage(this.index);
- 
- 
-    }
- }
- 
- class CarouselImage {
-    constructor(images) {
-        this.images = images;
-        this.images[0].style.display = "block";
- 
- 
-    }
-    displayNewImage(index) {
-        Array.from(this.images).forEach(image => image.style.display = "none");
-        this.images[index].style.display = "block";
-    }
- }
- 
- let carousel = document.querySelector('.carousel');
- let carouselObject = new Carousel(carousel);
- 
